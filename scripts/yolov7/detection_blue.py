f9d031c11713ec3fee1ecae966460488d3122e9a
JuanRobledo12 <jarobledo98@hotmail.com>
2023-05-16 18:30:33 -0400

    add yolov7 folder

---
+++ scripts/yolov7/detection_blue.py
@@ -0,0 +1,348 @@
+import numpy as np
+import datetime
+import os
+import argparse
+import time
+from pathlib import Path
+import pyrealsense2 as rs
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+from json_handler_class_edited import JSON_Handler
+
+# create instance of class
+json_handler = JSON_Handler(5, '/home/hello-robot/catkin_ws/src/blue_stretch/scripts/waypoint_info.json', new_json=False)
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+# # Configure depth and color streams
+# pipeline = rs.pipeline()
+# config = rs.config()
+# # Get device product line for setting a supporting resolution
+# pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+# pipeline_profile = config.resolve(pipeline_wrapper)
+# device = pipeline_profile.get_device()
+# device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+# found_rgb = False
+# for s in device.sensors:
+#     if s.get_info(rs.camera_info.name) == 'RGB Camera':
+#         found_rgb = True
+#         break
+# if not found_rgb:
+#     print("The demo requires Depth camera with Color sensor")
+#     exit(0)
+
+# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+# if device_product_line == 'L500':
+#     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+# else:
+#     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# # Start streaming
+# pipeline.start(config)
+# counter = 0
+# previous_x = 0
+# previous_y = 0
+
+
+def main():
+    img_counter = 0
+    rows, cols = (5,2)
+    timestamp_array = [[None] * cols]* rows
+    while(img_counter < 2):
+      
+        # Wait for frames
+        try:
+            frames = pipeline.wait_for_frames(timeout_ms = 5000)
+        except:
+            # Reset camera
+            print("reset start")
+            ctx = rs.context()
+            devices = ctx.query_devices()
+            for dev in devices:
+                dev.hardware_reset()
+                print("reset done")
+            frames = pipeline.wait_for_frames(timeout_ms = 5000)
+        color_frame = frames.get_color_frame()
+        color_image = np.asanyarray(color_frame.get_data())
+        frame = cv2.resize(color_image,(500,500))
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        img_counter = img_counter + 1
+
+        # Get time
+        timestamp_array[0][img_counter-1] = datetime.datetime.now()
+        date_time = timestamp_array[0][img_counter-1].strftime("%H_%M_%S")
+        filename = date_time + "_img_" + str(img_counter) + ".png"
+
+        # Save data
+        directory = "/home/hello-robot/yolov7/images_demo/waypoint1"
+        os.chdir(directory)
+        cv2.imwrite(filename,frame)
+        print('image saved',img_counter)
+
+    # Destroy all the windows
+    pipeline.stop()
+    cv2.destroyAllWindows()
+    new_project = "/home/hello-robot/yolov7/images_result/waypoint1"
+
+    for i in range(2):
+        img_no = str(i+1)
+
+        date_time = timestamp_array[0][img_counter-1].strftime("%H_%M_%S")
+        img_name = date_time + "_img_" + img_no + ".png"
+        new_source = "/home/hello-robot/yolov7/images_demo/waypoint1/" + img_name
+        new_project = "/home/hello-robot/yolov7/images_result/waypoint1"
+        detect(new_source,new_project,date_time,img_name)
+
+
+def detect(new_source,new_project, date_time,img_name,waypoint,save_img=True):
+    # source, project, weights, view_img, save_txt, imgsz, trace = new_source, new_project, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    # create instance of class
+    json_handler = JSON_Handler(5, '/home/hello-robot/catkin_ws/src/blue_stretch/scripts/waypoint_info.json', new_json=False)
+    source, project = new_source, new_project
+    
+    weights = '/home/hello-robot/yolov7/yolov7-tiny.pt'
+    imgsz = 640
+    img_size = 640
+    trace = False
+
+    # parser.add_argument('--weights', nargs='+', type=str, default='/home/hello-robot/yolov7/yolov7-tiny.pt', help='model.pt path(s)')
+    # parser.add_argument('--source', type=str, default='/home/hello-robot/yolov7/images_demo', help='source')  # file/folder, 0 for webcam
+    # parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    conf_thres=0.25
+    iou_thres=0.45
+    device='cpu'
+    view_img=False
+    save_txt=False
+    save_img = True
+    save_conf=True
+    nosave=False
+    classes=None
+    agnostic_nms = True
+    augment=True
+    update = True
+    # parser.add_argument('--project', default='/home/hello-robot/yolov7/images_result/waypoint1', help='save results to project/name')
+    name='/'
+    exist_ok = True
+    no_trace=False
+
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    
+    save_dir = Path(project, exist_ok=exist_ok)
+    (save_dir / '(labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=augment)[0]
+
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=augment)[0]
+        t2 = time_synchronized()
+
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
+        t3 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        objects_array = []
+        confidences_array = []
+
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / img_name)  # img.jpg
+            print(img_name)
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    for j in range(n):
+                        objects_array.append(f"{names[int(c)]}")
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+                    # Add confidence to array
+                    confidences_array.append(f'{conf:.2f}')
+
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+            # Add image data to json
+            new_project = new_project + "/" + img_name
+            w = waypoint
+            if w == 0:
+                json_handler.add_image_data('waypoint_1',new_project, date_time, objects_array, confidences_array)
+            elif w ==1:
+                json_handler.add_image_data('waypoint_2',new_project, date_time, objects_array, confidences_array)
+            elif w == 2:
+                json_handler.add_image_data('waypoint_3',new_project, date_time, objects_array, confidences_array)
+            elif w == 3:
+                json_handler.add_image_data('waypoint_4',new_project, date_time, objects_array, confidences_array)
+            elif w == 4:
+                json_handler.add_image_data('waypoint_5',new_project, date_time, objects_array, confidences_array)
+
+
+
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        #print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+        
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='/home/hello-robot/yolov7/yolov7-tiny.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='/home/hello-robot/yolov7/images_demo', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='/home/hello-robot/yolov7/images_result/waypoint1', help='save results to project/name')
+    parser.add_argument('--name', default='', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    # with torch.no_grad():
+    #     if opt.update:  # update all models (to fix SourceChangeWarning)
+    #         for opt.weights in ['yolov7.pt']:
+    #             detect()
+    #             strip_optimizer(opt.weights)
+    #     else:
+    #         detect()
+    main()
+
+# After the loop release the cap object
+#vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
